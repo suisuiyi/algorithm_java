a8260d14e237692344e7cd4a5d04539240496e55
BOLO <daiyangguang@yeah.net>
2022-07-24 13:31:44 +0800

    35. 搜索插入位置 代码优化

--- src/leetcode/day07/SearchInsert.java
+++ src/leetcode/day07/SearchInsert.java
@@ -2,10 +2,12 @@ package leetcode.day07;
 
 public class SearchInsert {
     public static void main(String[] args) {
-        int[] nums = new int[]{1,3,5,6};
-        int target = 4;
+        //35. 搜索插入位置
+        int[] nums = new int[]{1, 3, 5, 6};
+        int target = 7;
 
-        System.out.println(searchInsert(nums, target));
+//        System.out.println(searchInsert(nums, target));
+        System.out.println(searchInsertYouhua(nums, target));
     }
 
     public static int searchInsert(int[] nums, int target) {
@@ -14,20 +16,19 @@ public class SearchInsert {
         //3、考虑、头尾部插入问题
         int i;
         boolean isExist = true;
-        for(i =0; i < nums.length; i++) {
-            if(nums[i] == target) {
+        for (i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
                 return i;
             }
-            if(i == nums.length -1) {
+            if (i == nums.length - 1) {
                 isExist = false;
             }
         }
 
 
-
-        if(!isExist) {
-            for(i =0; i < nums.length; i++) {
-                if(nums[i] >= target ) {
+        if (!isExist) {
+            for (i = 0; i < nums.length; i++) {
+                if (nums[i] >= target) {
                     return i;
 
                 }
@@ -39,5 +40,15 @@ public class SearchInsert {
 
     }
 
+    public static int searchInsertYouhua(int[] nums, int target) {
 
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= target) {
+                return i;
+            }
+        }
+
+        return nums.length;
+
+    }
 }
